2406e2fe3b2b8a5bf82f7de5251a20834fe473ea
triyanarief <triyanarief@icloud.com>
2017-02-20 11:09:19 +0700

    done

--- models/cohort.js
+++ models/cohort.js
@@ -2,7 +2,93 @@
 
 import Student from "./student.js";
 
+let ADD_DATA ="INSERT INTO cohorts (name) VALUES (?)"
+let UPDATE_DATA = "UPDATE cohorts SET name=? WHERE id = ?"
+let DELETE_DATA ="DELETE FROM cohorts WHERE id = ?"
+let SEARCH_ID = "SELECT * FROM cohorts WHERE id= ?"
+
 class Cohort {
+  constructor(name,id){
+    this.name = name
+    this.id =id
+  }
+
+  static create(connection,cohort){
+    let c = connection
+    c.serialize(function(){
+      c.run(ADD_DATA,cohort.name,function(err){
+        if(err){
+          console.log(err);
+        } else{
+          console.log('cohort added');
+        }
+      })
+    })
+  }
+
+  static update(connection,cohort){
+    let d = connection
+    d.serialize(function(){
+      d.run(UPDATE_DATA,cohort.name,cohort.id,function(err){
+        if(err){
+          console.log(err);
+        } else{
+          console.log('cohort updated');
+        }
+      })
+    })
+  }
+
+  static delete(connection,id){
+    let d = connection
+    d.serialize(function(){
+      d.run(DELETE_DATA,id,function(err){
+        if(err){
+          console.log(err);
+        } else{
+          console.log('cohort deleted');
+        }
+      })
+    })
+  }
+
+  static findById(connection,id){
+  let d = connection
+  d.serialize(function(){
+    d.each(SEARCH_ID,id,function(err,row){
+      if(err){
+        console.log(err);
+      } else{
+        console.log(row);
+      }
+    })
+  })
+  }
+
+  static cb (err,data){
+      if(!err){
+        for(var i=0; i<data.length;i++){
+          console.log(data[i])
+        }
+      } else{
+        console.log("error")
+      }
+  }
+
+
+  static findAll(connection,call){
+    let c = connection
+    c.serialize(function(){
+      c.all("SELECT * FROM cohorts",call)
+    })
+  }
+
+  static where(connection,what,call){
+    let c = connection
+    c.serialize(function(){
+      c.all(`SELECT * FROM cohorts WHERE ${what}`,call)
+    })
+  }
 
 
 }
--- test/simple.js
+++ test/simple.js
@@ -2,3 +2,47 @@ import DBModel from "../models/db_model.js";
 import Student from "../models/student.js";
 
 var db = new DBModel("./db/test.db")
+
+function tesStudentCreate(){
+  var tes ={
+    firstname: 'heylo',
+    lastname: 'ceylo',
+    cohort_id : 1
+  }
+
+  Student.create(db.connection, new Student(tes.firstname,tes.lastname,tes.cohort_id))
+  cek(tes)
+}
+
+function tesStudentUpdate(){
+  var tesi ={
+    id : 5,
+    firstname: 'caylo',
+    lastname: 'cewlo',
+    cohort_id : 2
+  }
+
+  Student.update(db.connection, new Student(tesi.firstname,tesi.lastname,tesi.cohort_id,tesi.id))
+  cek(tesi)
+}
+
+function cek(tes){
+  var query= `SELECT * FROM students
+              WHERE firstname ='${tes.firstname}' AND lastname='${tes.lastname}'
+              AND cohort_id = ${tes.cohort_id}`
+
+  db.connection.serialize(function(){
+    db.connection.all(query,function(err,students){
+      if(!err && students.length>0){
+        console.log('tes create/update student: sucess')
+      }
+      else{
+        console.log('tes gagal');
+      }
+    })
+  })
+
+}
+
+tesStudentCreate();
+tesStudentUpdate();
